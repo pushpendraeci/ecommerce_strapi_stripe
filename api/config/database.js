6a0a9504fe70174e1e02184d9bb2fea6450a17c2
pushpendraeci <pushpendra.eci@gmail.com>
2023-02-15 17:07:12 +0530

    Update database.js

--- api/config/database.js
+++ api/config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
+      host: env('DATABASE_HOST', 'sql12.freemysqlhosting.net'),
       port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'strapi'),
-      user: env('DATABASE_USERNAME', 'root'),
-      password: env('DATABASE_PASSWORD', ''),
+      database: env('DATABASE_NAME', 'sql12598393'),
+      user: env('DATABASE_USERNAME', 'sql12598393'),
+      password: env('DATABASE_PASSWORD', 'JztmNw99zA'),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
